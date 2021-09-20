0ec26dda5890ba9439fc7c0855dd89e354d7ed41
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2021-09-19 22:39:47 -0600

    added add medication section

--- src/components/AddMedication.js
+++ src/components/AddMedication.js
@@ -10,14 +10,37 @@ function AddMedication() {
         setMedicine(target.value);
     }
     const handleClick = () => {
-        dispatch(add(medicine));
-        setMedicine('');
+        if (medicine !== '') {
+            dispatch(add(medicine));
+            setMedicine('');
+        }
     }
 
     return (
-        <div>
+        <div className="bg-main p-2 rounded">
             <h3>Add Medication</h3>
-            <input onChange={handleChange} value={medicine}/>
+            <div className="row">
+                <div className="col-md-3 col-sm-5">
+                    <label className="d-block">Medication Name
+                        <input className="d-block" onChange={handleChange} value={medicine}/>
+                    </label>
+                    <label className="d-block">Time Between Doses
+                        <input className="d-block" />
+                    </label>
+                    <label className="d-block">Take With Food?
+                        <span className="d-block"><input type="radio" value="yes" name="with-food" /> Yes</span>
+                        <span className="d-block"><input type="radio" value="no" name="with-food" /> No</span>
+                    </label>
+                </div>
+                <div className="col-md-3 col-sm-5">
+                    <label className="d-block">Generic Name
+                        <input className="d-block"/>
+                    </label>
+                    <label className="d-block">Comments
+                        <textarea className="d-block"></textarea>
+                    </label>
+                </div>
+            </div>
             <button onClick={handleClick}>Add To My Medication</button>
         </div>
     )
